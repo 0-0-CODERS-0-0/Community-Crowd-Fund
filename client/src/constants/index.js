86f15df8eca6f07854f401b1e0ed31985a6d1cc0
nasablackberry <singhshivank0605@gmail.com>
2023-03-11 02:29:38 +0530

    final

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -15,23 +15,10 @@ export const navlinks = [
     name: 'payment',
     imgUrl: payment,
     link: '/',
-    disabled: true,
-  },
-  {
-    name: 'withdraw',
-    imgUrl: withdraw,
-    link: '/',
-    disabled: true,
   },
   {
     name: 'profile',
     imgUrl: profile,
     link: '/profile',
-  },
-  {
-    name: 'logout',
-    imgUrl: logout,
-    link: '/',
-    disabled: true,
   },
 ];
